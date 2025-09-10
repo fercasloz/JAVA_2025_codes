74381b272f99aaf725450edda2ef99bae6f2e03f
Fernando Castillo Lozano <fernando25798@gmail.com>
2025-09-10 23:13:26 +0200

    codigo de sockets

---
+++ Sockets/src/fer/sockets/Cliente.java
@@ -0,0 +1,62 @@
+package fer.sockets;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+import java.net.*;
+
+public class Cliente {
+
+	public static void main (String[] args) {
+		MarcoCliente mimarco = new MarcoCliente();
+		mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
+
+class MarcoCliente extends JFrame{
+	
+	public MarcoCliente(){
+		
+		setBounds(600,300,280,350);	
+		LaminaMarcoCliente milamina=new LaminaMarcoCliente();
+		add(milamina);
+		setVisible(true);
+	}	
+}
+
+class LaminaMarcoCliente extends JPanel{
+	
+	public LaminaMarcoCliente() {
+		
+		JLabel texto = new JLabel("CLIENTE");
+		add(texto);
+		campo1 = new JTextField(20);
+		add(campo1);
+		miboton = new JButton("Enviar");
+		
+		EnviaTexto mievento = new EnviaTexto();
+		
+		miboton.addActionListener(mievento);
+		
+		add(miboton);
+	}
+	
+	private class EnviaTexto implements ActionListener{
+
+		public void actionPerformed(ActionEvent e) {
+			
+			//System.out.println(campo1.getText());
+			
+			//creamos el socket
+			
+			Socket misocket = new Socket("192.168.18.143",9999);
+			
+		}
+		
+	}
+	
+	
+	
+	private JTextField campo1;
+	private JButton miboton;
+}
